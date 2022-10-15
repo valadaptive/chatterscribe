c240e37a7a3fdcbbd7b015cd80687e9b3414e1ef
valadaptive <valadaptive@protonmail.com>
2022-10-15 12:52:07 -0400

    Convert CharacterList to hooks

--- src/components/CharacterList/CharacterList.tsx
+++ src/components/CharacterList/CharacterList.tsx
@@ -1,7 +1,8 @@
 import style from './style.scss';
 import icons from '../../icons/icons.scss';
 
-import {Component, JSX} from 'preact';
+import type {JSX} from 'preact';
+import {useMemo, useCallback} from 'preact/hooks';
 import classNames from 'classnames';
 
 import deleteCharacter from '../../actions/delete-character';
@@ -52,47 +53,23 @@ const connectedKeys = ['chars', 'currentCharID', 'convos', 'currentConvoIndex'] 
 const connectedActions = {deleteCharacter, setCurrentCharacterID, setEditedCharacterID, setToBeReplacedCharacterID};
 type Props = InjectProps<{}, typeof connectedKeys, typeof connectedActions>;
 
-class CharacterList extends Component<Props> {
-    constructor (props: Props) {
-        super(props);
-
-        this.onClickCharacter = this.onClickCharacter.bind(this);
-        this.onEditCharacter = this.onEditCharacter.bind(this);
-        this.onDeleteCharacter = this.onDeleteCharacter.bind(this);
-    }
-
-    onClickCharacter (character: Character): void {
-        this.props.setCurrentCharacterID(character.id);
-    }
-
-    onEditCharacter (character: Character): void {
-        this.props.setEditedCharacterID(character.id);
-    }
-
-    onDeleteCharacter (character: Character): void {
-        for (const convo of this.props.convos) {
-            for (const message of convo.messages) {
-                if (message.authorID === character.id) {
-                    this.props.setToBeReplacedCharacterID(character.id);
-                    return;
-                }
-            }
-        }
-
-        this.props.deleteCharacter(character.id);
-    }
-
-    render (): JSX.Element {
-        const {chars, currentCharID, convos, currentConvoIndex} = this.props;
-
+const CharacterList = ({
+    chars,
+    currentCharID,
+    convos,
+    currentConvoIndex,
+    deleteCharacter,
+    setCurrentCharacterID,
+    setEditedCharacterID,
+    setToBeReplacedCharacterID
+}: Props): JSX.Element => {
+    const {charsInConvo, charsNotInConvo} = useMemo(() => {
         const charIDsInConvo = new Set<ID>();
 
         if (currentConvoIndex !== -1) {
             const currentConvo = convos[currentConvoIndex];
             for (const message of currentConvo.messages) {
-                if (!charIDsInConvo.has(message.authorID)) {
-                    charIDsInConvo.add(message.authorID);
-                }
+                charIDsInConvo.add(message.authorID);
             }
         }
 
@@ -106,43 +83,66 @@ class CharacterList extends Component<Props> {
             }
         }
 
-        return (
-            <div className={style.characterList}>
-                {charsInConvo.length > 0 &&
-                    <>
-                        <div className={style.divider}>Characters in current convo:</div>
-                        <div className={style.characterSublist}>
-                            {charsInConvo.map(char =>
-                                <CharacterListing
-                                    key={char.id}
-                                    char={char}
-                                    active={char.id === currentCharID}
-                                    onClick={this.onClickCharacter}
-                                    onEdit={this.onEditCharacter}
-                                    onDelete={this.props.chars.length > 1 ? this.onDeleteCharacter : undefined}
-                                />)}
-                        </div>
-                    </>
+        return {charsInConvo, charsNotInConvo};
+    }, [currentConvoIndex, convos, chars]);
+
+    const onClickCharacter = useCallback((character: Character) => {
+        setCurrentCharacterID(character.id);
+    }, [setCurrentCharacterID]);
+
+    const onEditCharacter = useCallback((character: Character) => {
+        setEditedCharacterID(character.id);
+    }, [setEditedCharacterID]);
+
+    const onDeleteCharacter = useCallback((character: Character) => {
+        for (const convo of convos) {
+            for (const message of convo.messages) {
+                if (message.authorID === character.id) {
+                    setToBeReplacedCharacterID(character.id);
+                    return;
                 }
-                {charsNotInConvo.length > 0 &&
-                    <>
-                        <div className={style.divider}>Other characters:</div>
-                        <div className={style.characterSublist}>
-                            {charsNotInConvo.map(char =>
-                                <CharacterListing
-                                    key={char.id}
-                                    char={char}
-                                    active={char.id === currentCharID}
-                                    onClick={this.onClickCharacter}
-                                    onEdit={this.onEditCharacter}
-                                    onDelete={this.props.chars.length > 1 ? this.onDeleteCharacter : undefined}
-                                />)}
-                        </div>
-                    </>
-                }
-            </div>
-        );
-    }
-}
+            }
+        }
+
+        deleteCharacter(character.id);
+    }, [setToBeReplacedCharacterID, deleteCharacter, convos]);
+
+    return (
+        <div className={style.characterList}>
+            {charsInConvo.length > 0 &&
+                <>
+                    <div className={style.divider}>Characters in current convo:</div>
+                    <div className={style.characterSublist}>
+                        {charsInConvo.map(char =>
+                            <CharacterListing
+                                key={char.id}
+                                char={char}
+                                active={char.id === currentCharID}
+                                onClick={onClickCharacter}
+                                onEdit={onEditCharacter}
+                                onDelete={chars.length > 1 ? onDeleteCharacter : undefined}
+                            />)}
+                    </div>
+                </>
+            }
+            {charsNotInConvo.length > 0 &&
+                <>
+                    <div className={style.divider}>Other characters:</div>
+                    <div className={style.characterSublist}>
+                        {charsNotInConvo.map(char =>
+                            <CharacterListing
+                                key={char.id}
+                                char={char}
+                                active={char.id === currentCharID}
+                                onClick={onClickCharacter}
+                                onEdit={onEditCharacter}
+                                onDelete={chars.length > 1 ? onDeleteCharacter : undefined}
+                            />)}
+                    </div>
+                </>
+            }
+        </div>
+    );
+};
 
 export default connect(connectedKeys, connectedActions)(CharacterList);
